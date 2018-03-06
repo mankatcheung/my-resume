5a71d0e1627c63d4250672559b9d8ad0e5f96140
mankatcheung <jeffman231@hotmail.com>
2018-03-06 19:31:18 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Data from './data';
-
-ReactDOM.render(<App {...Data}/>, document.getElementById('root'));
-registerServiceWorker();
