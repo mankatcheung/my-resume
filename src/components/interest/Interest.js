610c16a258c5768f6b843a60d8d66cc823c68c89
mankatcheung <jeffman231@hotmail.com>
2018-03-08 01:58:39 +0800

    add alt to img

--- src/components/interest/Interest.js
+++ src/components/interest/Interest.js
@@ -17,7 +17,7 @@ export default class Interest extends Component {
         </div>
         <div className='Interest_Map'>
           <a href="https://www.adventureally.com/my-map-of-countries-visited?id=201803075a9fd402c4dd66.23066969">
-            <img src="https://s1.adventureally.com/dc/maps/2018/03/07/201803075a9fd402c4dd66.23066969.png"/>
+            <img src="https://s1.adventureally.com/dc/maps/2018/03/07/201803075a9fd402c4dd66.23066969.png" alt='Travel Map'/>
           </a>
         </div>
       </Paper>
