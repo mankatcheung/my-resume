6b35f3f22f226119af108f53bb1eb7cf789e1651
mankatcheung <jeffman231@hotmail.com>
2018-03-15 12:39:57 +0800

    add GA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Data from './data';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-104529883-2');
+ReactGA.pageview('my-resume');
 ReactDOM.render(<App {...Data}/>, document.getElementById('root'));
 registerServiceWorker();
